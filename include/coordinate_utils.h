88ca3a1aba2174512c66db8783a3d6180370541f
dbc201 <denizbcakiroglu@gmail.com>
2023-12-11 21:53:44 -0500

    add coordinate cache and move vector/string methods to string_utils.h

--- include/coordinate_utils.h
+++ include/coordinate_utils.h
@@ -1,39 +1,44 @@
 #ifndef COORDINATE_UTILS_H
 #define COORDINATE_UTILS_H
-#include "file_utils.h"
+
 #include "string_utils.h"
+#include <unordered_set>
 
 namespace cpp_utils::coordinate_utils {
 	template <typename T>
-	std::vector<T> string_to_coordinate_vector(std::string str) {
-		return cpp_utils::string_utils::convert_string_vector<T>(cpp_utils::string_utils::split_by_string(str, ","));
-	}
+	class CoordinateCache {
+	public:
+		CoordinateCache() = default;
 
-	template <typename T>
-	std::string coordinate_vector_to_string(std::vector<T> v) {
-		std::string str;
-		for (int i=0; i<v.size(); i++) {
-			str += std::to_string(v[i]);
-			if (i != v.size()-1) {
-				str += ",";
-			}
+		void insert(std::vector<T> coordinate) {
+			std::string coordinate_str = cpp_utils::string_utils::vector_to_string<T>(coordinate);
+			cache.insert(coordinate_str);
 		}
 
-		return str;
-	}
-
-	std::string coordinate_vector_to_string(std::vector<std::string> v) {
-		std::string str;
-		for (int i=0; i<v.size(); i++) {
-			str += v[i];
-			if (i != v.size()-1) {
-				str += ",";
-			}
+		bool contains(std::vector<T> coordinate) {
+			std::string coordinate_str = cpp_utils::string_utils::vector_to_string<T>(coordinate);
+			return cache.contains(coordinate_str);
 		}
 
-		return str;
-	}
+		void erase(std::vector<T> coordinate) {
+			std::string coordinate_str = cpp_utils::string_utils::vector_to_string<T>(coordinate);
+			cache.erase(coordinate_str);	
+		}
 
+		size_t size() {
+			return cache.size();
+		}
+
+		std::unordered_set<std::string> get_cache() {
+			return cache;
+		}
+
+		void clear() {
+			cache.clear();
+		}
+	private:
+		std::unordered_set<std::string> cache;
+	};
 } // cpp_utils::coordinate_utils
 
 #endif // COORDINATE_UTILS_H
--- include/string_utils.h
+++ include/string_utils.h
@@ -82,6 +82,36 @@ namespace cpp_utils::string_utils {
 		}
 		return true;
 	}
+
+	template <typename T>
+	std::vector<T> string_to_vector(std::string str) {
+		return convert_string_vector<T>(split_by_string(str, ","));
+	}
+
+	template <typename T>
+	std::string vector_to_string(std::vector<T> v) {
+		std::string str;
+		for (int i=0; i<v.size(); i++) {
+			str += std::to_string(v[i]);
+			if (i != v.size()-1) {
+				str += ",";
+			}
+		}
+
+		return str;
+	}
+
+	std::string vector_to_string(std::vector<std::string> v) {
+		std::string str;
+		for (int i=0; i<v.size(); i++) {
+			str += v[i];
+			if (i != v.size()-1) {
+				str += ",";
+			}
+		}
+
+		return str;
+	}
 } // cpp_utils::string_utils
 
 #endif // STRING_UTILS_H
